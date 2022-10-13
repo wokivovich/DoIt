2e42952e47526d2eb69d8a30ea3a9f3a4c0af08f
Alexander Nikulin <wokiwokivovich@gmail.com>
2022-10-13 17:37:33 +0300

    tm-3 Changed styles of main and login page

--- src/main/java/com/wokivovich/tm/config/WebSecurityConfig.java
+++ src/main/java/com/wokivovich/tm/config/WebSecurityConfig.java
@@ -19,6 +19,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .authorizeRequests().antMatchers("/style.css").permitAll().and()
                 .authorizeRequests()
                     .anyRequest().authenticated()
                 .and()
